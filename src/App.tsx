bd00ed7520d6c81d11b4daba973036dba4c56ae5
jacob-oreilly <jakeor45@gmail.com>
2026-01-18 15:53:39 -0800

    scraped the site and pulled down the images so we weren't constantly hitting BW cdn for the images.

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,28 @@ interface Card {
 }
 function App() {
   const artists = cards as Card[];
+  const modules = import.meta.glob<Record<string, any>>('./assets/artist_cards/*.{jpg,jpeg,png,webp}', { eager: true });
 
+  const images = Object.keys(modules).map((imagePath) =>{
+    return modules[imagePath].default;
+  });
+  
   return (
     <>
       <h2>Beyond Wonderland 2026 lineup so far...</h2>
       <div className='container'>
         <div className='grid-container'>
-          {artists.map((card) => {
+          {/* {artists.map((card) => {
             return (
             <div className='grid-item' key={card.card_id}>
               <img src={card.image} />
+            </div>
+            )
+          })} */}
+          {images.map((src, index) => {
+            return (
+            <div className='grid-item' key={index}>
+              <img src={src} alt={`Gallery image ${index + 1}`} />
             </div>
             )
           })}
